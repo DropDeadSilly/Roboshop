77ec8929d0c6683f1854d8c560ae25ae6183d39a
DropDeadSilly <vasantkumar.vemula@gmail.com>
2021-02-14 15:52:02 -0600

    Updating common.sh file

--- components/frontend.sh
+++ components/frontend.sh
@@ -5,7 +5,7 @@ COMPONENT=FrontEnd
 source components/common.sh
 
 Print "Installing Ngnix" "yum install nginx -y"
-yum install nginx -y &>/tmp/log
+yum install nginx -y
 Stat $?
 
 Print "Enabling Ngnix Service" "systemctl enable nginx"
@@ -16,10 +16,11 @@ Print "Starting Nginx Service" "systemctl start nginx"
 systemctl start nginx
 Stat $?
 
-exit
+Print "Downloading FrontEnd content" "'curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"'"
 curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/e869c1fe-489d-4f58-b676-fb75fe4e9e12/_apis/git/repositories/18fbb315-ed99-4a65-9b79-e7eec2f3e1b1/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+Stat $?
 
-
+exit
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
