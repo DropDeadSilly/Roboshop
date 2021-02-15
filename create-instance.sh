d9475ab90c02ca311f333ecfbdc08d864788c29b
DropDeadSilly <vasantkumar.vemula@gmail.com>
2021-02-14 19:47:17 -0600

    Updating create-instance.sh file

--- create-instance.sh
+++ create-instance.sh
@@ -9,8 +9,8 @@ fi
 
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
-if [ "$STATE" != 'running' ]; then
-  aws ec2 run-instances --launch-template LaunchTemplateId=lt-0e2dc0c3d69abc55e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+if [ "$STATE" != "running" ]; then
+  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0c43aaeb0e08199e0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
   sleep 15
 fi
 
