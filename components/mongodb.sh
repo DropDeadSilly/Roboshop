0357deaf7c0462bebcb55aac4eef06c27596a260
DropDeadSilly <vasantkumar.vemula@gmail.com>
2021-02-14 21:14:16 -0600

    Updating create-instance.sh file

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -37,9 +37,6 @@ Print "Downloading MongoDB Schema" "Downlaod URL --XXX--"
 curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/03f2af34-e227-44b8-a9f2-c26720b34942/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 Stat $?
 
-Print "Extracting & Loading Mongo Schema to MongoDB" "unzip mongodb.zip && mongo < catalogue.js && mongo < users.js"
-cd /tmp
-unzip mongodb.zip
-mongo < catalogue.js
-mongo < users.js
+Print "Extracting & Loading Mongo Schema to MongoDB" "cd /tmp && unzip -o mongodb.zip && mongo < catalogue.js && mongo < users.js"
+cd /tmp && unzip -o mongodb.zip && mongo < catalogue.js && mongo < users.jscd /tmp && unzip -o mongodb.zip && mongo < catalogue.js && mongo < users.js
 Stat $?
