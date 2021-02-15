ba80dd435a08ae7d95c9e89f2d960188070d4fe9
DropDeadSilly <vasantkumar.vemula@gmail.com>
2021-02-15 17:13:21 -0600

    Updating redis.sh file

--- components/redis.sh
+++ components/redis.sh
@@ -13,8 +13,8 @@ Print "Enabling Redis Repos" "yum-config-manager --enable remi && yum install re
 yum-config-manager --enable remi && yum install redis -y
 Stat $?
 
-Print "Update Redis Config" "sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf
+Print "Update Redis Config" "sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 Stat $?
 
 Print "Restarting Redis service" "systemctl enable redis && systemctl restart redis"
