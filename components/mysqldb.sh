8b54b932eb8e150c2cad230f6dbec389fd9357e4
DropDeadSilly <vasantkumar.vemula@gmail.com>
2021-02-20 19:32:12 -0600

    Updating mysqldb.sh file

--- components/mysqldb.sh
+++ components/mysqldb.sh
@@ -35,6 +35,8 @@ Stat $?
 #  Stat $?
 #fi
 
+echo "Test2"
+
 echo "show databases;" | mysql -uroot -ppassword &>/dev/null
 if [ $? -ne 0 ]; then
   Print "Grab Default MySQL Password" "grep temp /var/log/mysqld.log"
@@ -50,7 +52,7 @@ EOF
   Stat $?
 fi
 
-echo "Test123!"
+echo "Test2"
 
 Print "Download Schema" 'curl -s -L -o /tmp/mysql.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/2235ab8a-3229-47d9-8065-b56713ed7b28/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"'
 
